9f7ab03035beff69c54a96529e97c49b7b3ef99d
Jishnu <jishnu7@gmail.com>
2012-08-14 22:10:48 +0530

    pep8 cleaning

--- spellchecker/indexer.py
+++ spellchecker/indexer.py
@@ -29,89 +29,86 @@ class DictionaryIndex:
         self.op = None
         self.dictionary = dict()
         self.offset = 0
-        self.path = os.path.join(os.path.dirname(__file__),"dicts")
+        self.path = os.path.join(os.path.dirname(__file__), "dicts")
         self.dictionary_file = None
         self.index_file = None
 
-    def create_index(self,dictfile):
+    def create_index(self, dictfile):
         """
-       
-       Creates index for a dictionary index file is created in form of 
-       dictionary object and creates a file by name dictfile.index with
-       contents in following format
-       A=1
-       B=2000 ....
-       (For eg. en_US.index)
-       
-       @param dictfile : name of the dictionary for which index should be created
+        Creates index for a dictionary index file is created in form of
+        dictionary object and creates a file by name dictfile.index with
+        contents in following format
+        A=1
+        B=2000 ....
+        (For eg. en_US.index)
 
+        @param dictfile : name of the dictionary for which
+        index should be created
         """
 
         self.dictionary_file = dictfile
         self.index_file = os.path.join(dictfile.split(".")[0] + ".index")
 
-
-        self.fp = codecs.open(self.dictionary_file,"r",encoding="utf-8")
-        self.op = codecs.open(self.index_file,"w",encoding="utf-8")
+        self.fp = codecs.open(self.dictionary_file, "r", encoding="utf-8")
+        self.op = codecs.open(self.index_file, "w", encoding="utf-8")
 
         # loop untill entire file is not finished
         while True:
             item = self.fp.readline()
             if not item:
                 break
-            
-            # if the alphabet is currently not indexed then index it 
+
+            # if the alphabet is currently not indexed then index it
             # with current value of byte offset else increase the offset
-            # by the byte length of currently read word till you get 
-	    # new alphaet which is not indexed
-    
-            if len(item)>0 and not self.dictionary.has_key(item[0]):
+            # by the byte length of currently read word till you get
+            # new alphaet which is not indexed
+
+            if len(item) > 0 and not self.dictionary.has_key(item[0]):
                 self.dictionary[item[0]] = self.offset
-            self.offset = self.offset + len(item.encode( "utf-8" ))
-                
-            
+            self.offset = self.offset + len(item.encode("utf-8"))
+
         #print "Index for " + self.dictionary_file + " is created "
 
         for index in self.dictionary:
-           value = self.dictionary.get(index,None)
-           if not value == None:
-                self.op.write(index + "=%d\n"% value)
-    
-    
+            value = self.dictionary.get(index, None)
+            if not value == None:
+                self.op.write(index + "=%d\n" % value)
+
         # Clean up
         self.fp.close()
         self.op.close()
 
-
-    def load_index(self,dictfile):
+    def load_index(self, dictfile):
         """
             This function reads the index file and loads the content into
             a dictionary object. If file doesn't exist this will create the
             index file and then reads it.
-            @param dictfile: Dictionary for which the index file is to be loaded
+            @param dictfile: Dictionary for which the index file is to be
+            loaded
             returns - dictionary object containing indexing information
         """
-    
-        self.index_file = os.path.join(self.path,dictfile.split(".")[0] + ".index")
+
+        self.index_file = os.path.join(self.path, \
+                dictfile.split(".")[0] + ".index")
         try:
-            self.fp = codecs.open(self.index_file,"r",encoding="utf-8",errors="ignore")
+            self.fp = codecs.open(self.index_file, "r", \
+                    encoding="utf-8", errors="ignore")
         except IOError:
             self.create_index(dictfile)
-       
-        self.fp = codecs.open(self.index_file,"r",encoding="utf-8")
+
+        self.fp = codecs.open(self.index_file, "r", encoding="utf-8")
         self.dictionary = {}
         while True:
             text = unicode(self.fp.readline())
             if text:
                 line = text.split("=")
-                if len(line)==2:
+                if len(line) == 2:
                     index = line[0]
                     value = line[1]
                     self.dictionary[index] = value
             else:
                 break
 
-        
         self.fp.close()
         return self.dictionary
 
