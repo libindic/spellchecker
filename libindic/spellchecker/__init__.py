37168aeeae6cfdd42ca2b56453bdbf4571371972
Balasankar C <balasankarc@autistici.org>
2016-06-25 13:39:21 +0530

    Initialize class structure and necessary methods

---
+++ libindic/spellchecker/__init__.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+import marisa_trie
+from libindic.stemmer import Malayalam as Stemmer
+
+
+class Malayalam:
+
+    def __init__(self):
+        '''
+        Initialize necessary resources.
+        '''
+        self.dictionary_file = os.path.join(
+            os.path.dirname(__file__), 'data/ml_rootwords.txt')
+        self.dictionary = self.dictionary_file.readlines()
+        self.dictionary_file.close()
+        try:
+            self.dictionary = marisa_trie.Trie([x.strip().decode('utf-8')
+                                                for x in self.dictionary])
+        except:
+            self.dictionary = marisa_trie.Trie(
+                [x.strip() for x in self.dictionary])
+        self.stemmer = Stemmer()
+
+    def check(self, word):
+        '''
+        Returns if a word is spelled correctly or not.
+        '''
+        pass
+
+    def suggest(self, word, n=10):
+        '''
+        Returns n suggestions that is similar to word.
+        '''
+        pass
+
+    def compare(self, word1, word2):
+        '''
+        Returns the similarity measure between two words.
+        '''
+        pass
